e59ccc4fa5fe5d556c904c40521c29adee8c6ae1
Tu Nombre <100297463+zizicode@users.noreply.github.com>
2026-08-29 00:21:55 -0400

    feat: add Google Analytics 4 tag (G-WPYBE6LB6M)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -72,6 +72,12 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KKPQ58WC');`,
           }}
         />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-WPYBE6LB6M" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-WPYBE6LB6M');`,
+          }}
+        />
         <link rel="manifest" href="/site.webmanifest" />
         <script
           type="application/ld+json"
