667dcb12c84f92152c973b72aaeda19707081a09
Tu Nombre <100297463+zizicode@users.noreply.github.com>
2026-08-29 00:16:35 -0400

    feat: add Google Tag Manager (GTM-KKPQ58WC) to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -67,6 +67,11 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html lang="en" data-scroll-behavior="smooth" className={`${geistSans.variable} ${geistMono.variable}`} suppressHydrationWarning>
       <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KKPQ58WC');`,
+          }}
+        />
         <link rel="manifest" href="/site.webmanifest" />
         <script
           type="application/ld+json"
@@ -78,6 +83,14 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
         />
       </head>
       <body suppressHydrationWarning>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KKPQ58WC"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <AppPreloader />
         <GlobalLoader />
         <div className="app-root">
